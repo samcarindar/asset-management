d5f2f62c8ff9ec16a1a7a75a94855fe05c2bc7bd
Suchanon Aegkathat <70258649+samcarindar@users.noreply.github.com>
2022-02-22 20:49:52 +0700

    update

--- librarys.sql
+++ librarys.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2022 at 11:22 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Generation Time: Feb 22, 2022 at 02:48 PM
+-- Server version: 10.6.4-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,12 +29,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `book` (
   `b_id` int(11) NOT NULL,
-  `b_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `b_name` varchar(50) NOT NULL,
   `c_id` int(11) NOT NULL,
-  `status` varchar(11) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'ปกติ/ว่าง',
+  `status` varchar(11) NOT NULL DEFAULT 'ปกติ/ว่าง',
   `price` int(11) NOT NULL,
-  `book_id` varchar(20) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `book_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `book`
@@ -42,7 +42,7 @@ CREATE TABLE `book` (
 
 INSERT INTO `book` (`b_id`, `b_name`, `c_id`, `status`, `price`, `book_id`) VALUES
 (5, 'ภาษาไทย', 1, 'ยืม/ใช้งาน', 230, '31002'),
-(6, 'วิทยาศาสตร์', 2, 'ยืม/ใช้งาน', 230, '31004'),
+(6, 'วิทยาศาสตร์', 2, 'ปกติ/ว่าง', 230, '31004'),
 (7, 'ท่องโลกกว้าง', 2, 'ปกติ/ว่าง', 360, '31005');
 
 -- --------------------------------------------------------
@@ -53,21 +53,27 @@ INSERT INTO `book` (`b_id`, `b_name`, `c_id`, `status`, `price`, `book_id`) VALU
 
 CREATE TABLE `booking` (
   `id` int(11) NOT NULL,
-  `book_id` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `book_id` varchar(20) NOT NULL,
   `c_id` int(11) NOT NULL,
   `start_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `end_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `price` int(11) NOT NULL,
-  `m_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `m_id` int(11) NOT NULL,
+  `status` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `booking`
 --
 
-INSERT INTO `booking` (`id`, `book_id`, `c_id`, `start_date`, `end_date`, `price`, `m_id`) VALUES
-(1, '31002', 1, '2022-02-22 09:58:20', '2022-02-25 17:00:00', 230, 2),
-(2, '31004', 2, '2022-02-22 09:59:47', '2022-02-25 17:00:00', 230, 1);
+INSERT INTO `booking` (`id`, `book_id`, `c_id`, `start_date`, `end_date`, `price`, `m_id`, `status`) VALUES
+(1, '31002', 1, '2022-02-22 13:28:37', '2022-02-25 17:00:00', 230, 2, 1),
+(2, '31004', 2, '2022-02-22 13:28:41', '2022-02-25 17:00:00', 230, 2, 1),
+(3, '31005', 2, '2022-02-22 13:28:43', '2022-02-25 17:00:00', 360, 2, 1),
+(4, '31002', 1, '2022-02-22 13:41:32', '2022-02-25 17:00:00', 230, 2, 1),
+(5, '31002', 1, '2022-02-22 13:43:54', '2022-02-25 17:00:00', 230, 2, 1),
+(6, '31004', 2, '2022-02-22 13:43:58', '2022-02-25 17:00:00', 230, 2, 1),
+(7, '31005', 2, '2022-02-22 13:44:01', '2022-02-25 17:00:00', 360, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -77,9 +83,9 @@ INSERT INTO `booking` (`id`, `book_id`, `c_id`, `start_date`, `end_date`, `price
 
 CREATE TABLE `category` (
   `c_id` int(11) NOT NULL,
-  `c_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `c_number` varchar(10) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `c_name` varchar(50) NOT NULL,
+  `c_number` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `category`
@@ -97,14 +103,14 @@ INSERT INTO `category` (`c_id`, `c_name`, `c_number`) VALUES
 
 CREATE TABLE `member` (
   `m_id` int(11) NOT NULL,
-  `m_card_id` varchar(13) COLLATE utf8_unicode_ci NOT NULL,
-  `m_name` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
-  `username` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `address` text COLLATE utf8_unicode_ci NOT NULL,
-  `phone` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `m_card_id` varchar(13) NOT NULL,
+  `m_name` varchar(60) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `address` mediumtext NOT NULL,
+  `phone` varchar(10) NOT NULL,
   `status` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `member`
@@ -156,7 +162,7 @@ ALTER TABLE `book`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `category`
