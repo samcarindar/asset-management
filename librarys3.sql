cb050972f44783ca0a1a91dde1b865d52e13c5c4
samcarindar <samcarindar1@gmail.com>
2022-02-22 17:23:18 +0700

    update

--- librarys3.sql
+++ librarys3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2022 at 11:09 AM
+-- Generation Time: Feb 22, 2022 at 11:22 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `librarys`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `b_id` int(11) NOT NULL,
+  `b_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `status` varchar(11) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'ปกติ/ว่าง',
+  `price` int(11) NOT NULL,
+  `book_id` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`b_id`, `b_name`, `c_id`, `status`, `price`, `book_id`) VALUES
+(5, 'ภาษาไทย', 1, 'ยืม/ใช้งาน', 230, '31002'),
+(6, 'วิทยาศาสตร์', 2, 'ยืม/ใช้งาน', 230, '31004'),
+(7, 'ท่องโลกกว้าง', 2, 'ปกติ/ว่าง', 360, '31005');
 
 -- --------------------------------------------------------
 
@@ -45,9 +69,60 @@ INSERT INTO `booking` (`id`, `book_id`, `c_id`, `start_date`, `end_date`, `price
 (1, '31002', 1, '2022-02-22 09:58:20', '2022-02-25 17:00:00', 230, 2),
 (2, '31004', 2, '2022-02-22 09:59:47', '2022-02-25 17:00:00', 230, 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `c_id` int(11) NOT NULL,
+  `c_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `c_number` varchar(10) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`c_id`, `c_name`, `c_number`) VALUES
+(1, 'ภาษาไทย', '003'),
+(2, 'วิทยาศาสตร์', '004');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `m_id` int(11) NOT NULL,
+  `m_card_id` varchar(13) COLLATE utf8_unicode_ci NOT NULL,
+  `m_name` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `address` text COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`m_id`, `m_card_id`, `m_name`, `username`, `password`, `address`, `phone`, `status`) VALUES
+(1, '1234567891234', 'admin', 'admin', '1234', '22/23', '0898765432', 1),
+(2, '1234567891234', 'test', 'test', '1234', '22/28', '0987654235', 0);
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`b_id`);
 
 --
 -- Indexes for table `booking`
@@ -56,14 +131,44 @@ ALTER TABLE `booking`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`m_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
